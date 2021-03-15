ea394ff53c4924d6378eac2186ce64cf112b5fd8
EnasBatayneh <enas_z_bataineh108@yahoo.com>
2021-03-15 16:50:19 +0200

    new

--- lab02.js
+++ lab02.js
@@ -11,28 +11,37 @@ if (type.toLocaleLowerCase() === 'yes' || type.toLocaleLowerCase() === 'y')
   let gender= prompt("Iam a 40 years old female!");
   if (gender.toLocaleLowerCase() === 'yes' || gender.toLocaleLowerCase() === 'y')
   {alert("Ooh, that's not true, Iam 27years old female");}
-  else{ alert("you guessed that, Iam not ");}
+  else if (gender.toLocaleLowerCase() === 'no' || gender.toLocaleLowerCase() === 'n')
+  { alert("you guessed that, Iam not ");}
 
   let career= prompt("Iam a prof. programmer");
   if (career.toLocaleLowerCase() === 'yes' || career.toLocaleLowerCase() === 'y')
   {alert("unfortunutly No! Iam still a beginner and try to be that");}
-  else{ alert("you guessed that, Iam still a student ");}
+  else if (career.toLocaleLowerCase() === 'no' || career.toLocaleLowerCase() === 'n')
+
+  { alert("you guessed that, Iam still a student ");}
 
   let prefer= prompt("I prefer an online shopping mode");
   if (prefer.toLocaleLowerCase() === 'yes' || prefer.toLocaleLowerCase() === 'y')
   {alert("Oh Oh, absolutly no, because I faced a previous bad experiences, unfortunutly!!");}
-  else{ alert("exactly, I prefer the in store shopping mode");}
+  else if (prefer.toLocaleLowerCase() === 'no' || prefer.toLocaleLowerCase() === 'n')
+
+  { alert("exactly, I prefer the in store shopping mode");}
 
   let colory= prompt("My favourite color is black");
   if (colory.toLocaleLowerCase() === 'yes' || colory.toLocaleLowerCase() === 'y')
   {alert("No, it is the pink");}
-  else{ alert("yeah, it is not my favourite one");}
+  else if (colory.toLocaleLowerCase() === 'no' || colory.toLocaleLowerCase() === 'n')
+
+  { alert("yeah, it is not my favourite one");}
 
 
   let sweets= prompt("the sweets is my lovely snack");
   if (sweets.toLocaleLowerCase() === 'yes' || sweets.toLocaleLowerCase() === 'y')
   {alert("yeah, I really love it");}
-  else{ alert("you didn't guess that, actually I prefer it");}
+  else if (sweets.toLocaleLowerCase() === 'no' || sweets.toLocaleLowerCase() === 'n')
+
+  { alert("you didn't guess that, actually I prefer it");}
 }
 else { alert("you can try another time");}
 
